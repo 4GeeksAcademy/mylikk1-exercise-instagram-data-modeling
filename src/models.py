dd2ebbacabdb6dabe3acdbaa146c8e2426eec73d
My'lik Kerley <159506912+KerleyCode@users.noreply.github.com>
2024-06-04 00:30:45 +0000

    updated

--- src/models.py
+++ src/models.py
@@ -7,28 +7,68 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# class Person(Base):
+#     __tablename__ = 'person'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    phone = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    icon = Column(String(250), nullable=False)
+    comments = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer,ForeignKey('post.id'))
+    post = relationship(Post)
 
-## Draw from SQLAlchemy base
+class Reactions(Base):
+    __tablename__ = 'reactions'
+    id = Column(Integer, primary_key=True)
+    number_of_likes = Column(Integer, nullable=False)
+    number_of_dislikes = Column(Integer, nullable=False)
+    post_id = Column(Integer,ForeignKey('post.id'))
+    post = relationship(Post)
+    comment_id = Column(Integer,ForeignKey('comments.id'))
+    comment = relationship(Comments)
+
+
+
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
+
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
